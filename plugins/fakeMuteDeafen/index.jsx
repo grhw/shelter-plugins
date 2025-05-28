2b86cdf8d920fc8ae291a61c8cbd57eee0003464
Gust Who? <lucgustga@outlook.com>
2025-05-29 01:10:35 +0800

    THEY'RE DOING IT AGAIN

--- plugins/fakeMuteDeafen/index.jsx
+++ plugins/fakeMuteDeafen/index.jsx
@@ -14,25 +14,6 @@ function createButton() {
 		button.classList.add("guhw-fmd")
 		button.setAttribute("aria-label","Lock Voice State")
 		button.querySelector("svg").innerHTML = `<path fill="currentColor" fill-rule="evenodd" d="M6 9h1V6a5 5 0 0 1 10 0v3h1a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-8a3 3 0 0 1 3-3Zm9-3v3H9V6a3 3 0 1 1 6 0Zm-1 8a2 2 0 0 1-1 1.73V18a1 1 0 1 1-2 0v-2.27A2 2 0 1 1 14 14Z" clip-rule="evenodd" class=""></path>`
-		window.fakeDeaf = ()=>{
-			button.classList.toggle("guhw-fmd-activated")
-			if (button.classList.contains("guhw-fmd-activated")) {
-				shelter.ui.showToast({
-					title: "Fake Mute-Deafen",
-					content: "Locked current voice state. Clicking Mute/Deaf will unmute/undeafen you, but it wont update the icons anywhere."
-				})
-				
-				store.deafened = stores.MediaEngineStore.isSelfDeaf()
-				store.muted = stores.MediaEngineStore.isSelfMute()
-				store.active = true
-			} else {
-				shelter.ui.showToast({
-					title: "Fake Mute-Deafen",
-					content: "Unlocked current voice state."
-				})
-				store.active = false
-			}
-		}
 		button.addEventListener("click",window.fakeDeaf())
 	}
 	return button
@@ -42,7 +23,26 @@ var unpatch_deaf
 var unpatch_mute
 var remove_css
 export function onLoad() {
-	log("[fakeMuteDeafen] loaded :3")
+	log("[fakeMuteDeafen] loaded >:3")
+	window.fakeDeaf = ()=>{
+		button.classList.toggle("guhw-fmd-activated")
+		if (button.classList.contains("guhw-fmd-activated")) {
+			shelter.ui.showToast({
+				title: "Fake Mute-Deafen",
+				content: "Locked current voice state. Clicking Mute/Deaf will unmute/undeafen you, but it wont update the icons anywhere."
+			})
+			
+			store.deafened = stores.MediaEngineStore.isSelfDeaf()
+			store.muted = stores.MediaEngineStore.isSelfMute()
+			store.active = true
+		} else {
+			shelter.ui.showToast({
+				title: "Fake Mute-Deafen",
+				content: "Unlocked current voice state."
+			})
+			store.active = false
+		}
+	}
 
 	unpatch_deaf = shelter.patcher.after("isSelfDeaf",stores.MediaEngineStore,(args,ret)=>{
 		if (store.active) {
